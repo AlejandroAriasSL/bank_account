ca162351832e80ac0d450b6cc1c9623f341da308
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-31 07:51:49 +0200

    add test_overdraft_initialzes_to_0() (tdd:red)

---
+++ src/test/java/dev/alejandro/models/CurrentAccountTest.java
@@ -0,0 +1,22 @@
+package dev.alejandro.models;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CurrentAccountTest {
+    
+    @Test
+    @DisplayName("It should have an attribute 'overdraft' initialized to 0")
+    void test_overdraft_initialzes_to_0() {
+
+        float balance = 20000;
+        float annualRate = 1; 
+
+        CurrentAccount currentAccount = new CurrentAccount(balance, annualRate);
+
+        assertThat(currentAccount.getOverdraft(), is(0.0f));
+    }
+}
