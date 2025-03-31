10fc5c3aa1eff3e9ae6ba8e8c465d500a0c988f0
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-31 08:07:38 +0200

    refactor tests with beforeEach setup and deleted redundant test

--- src/test/java/dev/alejandro/models/CurrentAccountTest.java
+++ src/test/java/dev/alejandro/models/CurrentAccountTest.java
@@ -3,47 +3,37 @@ package dev.alejandro.models;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CurrentAccountTest {
-    
+
+    private float balance;
+    private float annualRate;
+    private CurrentAccount currentAccount;
+
+    @BeforeEach
+    private void setUp() {
+
+        balance = 20000;
+        annualRate = 1;
+
+        currentAccount = new CurrentAccount(balance, annualRate);
+    }
+
     @Test
     @DisplayName("It should have an attribute 'overdraft' initialized to 0")
     void test_overdraft_initialzes_to_0() {
 
-        float balance = 20000;
-        float annualRate = 1; 
-
-        CurrentAccount currentAccount = new CurrentAccount(balance, annualRate);
-
         assertThat(currentAccount.getOverdraft(), is(0.0f));
-    }
-
-    @Test
-    @DisplayName("It should be able to withdraw amounts higher than balance")
-    void test_can_withdraw_amount_higher_than_balance() {
-
-        float balance = 20000;
-        float annualRate = 1; 
-        float amount = 30000;
-
-        CurrentAccount currentAccount = new CurrentAccount(balance, annualRate);
-
-        currentAccount.withdraw(amount);
-
-        assertThat(currentAccount.getBalance(), is(balance - amount));
     }
 
     @Test
     @DisplayName("It should accumulate debt as overdraft")
     void test_it_accumulates_debt_as_overdraft() {
 
-        float balance = 20000;
-        float annualRate = 1; 
         float amount = 30000;
-
-        CurrentAccount currentAccount = new CurrentAccount(balance, annualRate);
 
         currentAccount.withdraw(amount);
 
