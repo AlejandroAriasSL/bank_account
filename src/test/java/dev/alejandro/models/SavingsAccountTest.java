ff957c7855d47e109e38437b929ae28fa6ef7481
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-31 06:05:09 +0200

    add test_throws_exception_if_account_is_inactive_and_withdraw() (tdd:red)

--- src/test/java/dev/alejandro/models/SavingsAccountTest.java
+++ src/test/java/dev/alejandro/models/SavingsAccountTest.java
@@ -24,7 +24,7 @@ public class SavingsAccountTest {
     }
 
     @Test
-    @DisplayName("It should throw an exception if deposit is and account is inactive")
+    @DisplayName("It should throw an exception if deposit and account is inactive")
     void test_throws_exception_if_account_is_inactive() {
 
         float initialBalance = 5000;
@@ -57,5 +57,24 @@ public class SavingsAccountTest {
 
         assertTrue(savingsAccount.isActive());
         assertThat(savingsAccount.getBalance(), is(initialBalance + depositAmount));
+    }
+
+    @Test
+    @DisplayName("It should throw an exception if withdraw and account is inactive")
+    void test_throws_exception_if_account_is_inactive_and_withdraw() {
+
+        float initialBalance = 5000;
+        float annualRate = 1;
+        float withdrawAmount = 1000;
+
+        SavingsAccount savingsAccount = new SavingsAccount(initialBalance, annualRate);
+
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, () -> 
+            savingsAccount.withdraw(withdrawAmount));
+  
+
+        assertFalse(savingsAccount.isActive());
+        assertThat(exception.getMessage(), is("Cannot withdraw from an inactive account"));
     }
 }
