8d841b05c977ed9881b95bc371c68263c0b57e43
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-31 05:09:46 +0200

    add test_returns_active_status_of_account() (tdd:green)

---
+++ src/main/java/dev/alejandro/models/SavingsAccount.java
@@ -0,0 +1,14 @@
+package dev.alejandro.models;
+
+public class SavingsAccount extends Account {
+
+    private boolean active;
+
+    public SavingsAccount(float balance, float annualRate) {
+        super(balance, annualRate);
+    }
+
+    public boolean isActive() {
+        return getBalance() >= 10000.0f;
+    }
+}
