5e113bb8953940c0cd7284ac39413bd0363da9b9
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-31 07:53:30 +0200

    add test_overdraft_initialzes_to_0() (tdd:green)

---
+++ src/main/java/dev/alejandro/models/CurrentAccount.java
@@ -0,0 +1,14 @@
+package dev.alejandro.models;
+
+public class CurrentAccount extends Account {
+    private float overdraft;
+
+    public CurrentAccount(float balance, float annualRate) {
+        super(balance, annualRate);
+        this.overdraft = 0;
+    }
+
+    public float getOverdraft() {
+        return overdraft;
+    }
+}
